b6913a05952ee2e11cf8c73788445f950e44c14b
Walace Pavão <walacespo@gmail.com>
2022-07-22 18:48:10 -0300

    DTO

--- backend/src/main/java/com/walace/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/walace/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.walace.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.walace.dscatalog.dto.CategoryDTO;
 import com.walace.dscatalog.entities.Category;
 import com.walace.dscatalog.repositories.CategoryRepository;
 
@@ -14,9 +17,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
-		
+	public List<CategoryDTO> findAll(){
+		List<Category> list =  repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }

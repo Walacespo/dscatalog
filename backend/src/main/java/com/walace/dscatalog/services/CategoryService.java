64c6354b904d1681fa39ac3f7746377ca6874a02
Walace Pavão <walacespo@gmail.com>
2022-07-26 19:29:00 -0300

    Delete category

--- backend/src/main/java/com/walace/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/walace/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.walace.dscatalog.dto.CategoryDTO;
 import com.walace.dscatalog.entities.Category;
 import com.walace.dscatalog.repositories.CategoryRepository;
+import com.walace.dscatalog.services.exceptions.DatabaseException;
 import com.walace.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
@@ -55,6 +58,18 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }

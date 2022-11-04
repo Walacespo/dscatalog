2f5141cb074202582fa0298cdd10895366ff592f
Walace da Silva <walacespo@gmail.com>
2022-11-04 15:21:21 -0300

    File upload

--- backend/src/main/java/com/walace/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/walace/dscatalog/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.walace.dscatalog.services;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +14,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.walace.dscatalog.dto.CategoryDTO;
 import com.walace.dscatalog.dto.ProductDTO;
+import com.walace.dscatalog.dto.UriDTO;
 import com.walace.dscatalog.entities.Category;
 import com.walace.dscatalog.entities.Product;
 import com.walace.dscatalog.repositories.CategoryRepository;
@@ -25,6 +28,9 @@ import com.walace.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	@Autowired
 	private ProductRepository repository;
@@ -93,5 +99,10 @@ public class ProductService {
 			Category category = categoryRepository.getOne(catDto.getId());
 			entity.getCategories().add(category);			
 		}
+	}
+
+	public UriDTO uploadFile(MultipartFile file) {
+		URL url = s3Service.uploadFile(file);
+		return new UriDTO(url.toString());
 	}	
 }
